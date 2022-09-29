29ce7db8ddf3b4692e3d361945a39d71862b1b7d
ManuelagDuque <grajalesduquemanuela@gmail.com>
2022-09-29 23:25:36 +0200

    git ignore prepared to rect app

--- Backend_app/dev/config/constants_db.py
+++
@@ -1,9 +0,0 @@
-MY_SQL_HOST = "localhost"
-MY_SQL_USER = "root"
-MY_SQL_PASSWORD = "dba"
-
-MY_SQL_DETAILS = {
-    "host": MY_SQL_HOST,
-    "user": MY_SQL_USER,
-    "password": MY_SQL_PASSWORD
-}
