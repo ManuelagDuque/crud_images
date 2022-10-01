dee90eea5101a3d0216ba77ecdd45d84c8a6c7f5
ManuelagDuque <grajalesduquemanuela@gmail.com>
2022-10-02 00:46:40 +0200

    card styles

--- images_front_app/src/components/Home.jsx
+++ images_front_app/src/components/Home.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import useImagesAPI from '../hooks/useImagesAPI';
+import {TrashIcon, PencilIcon, StarIcon} from '@primer/octicons-react'
 
 function Home() {
     let url = 'http://127.0.0.1:8000/image' 
     let {data} = useImagesAPI(url)
     console.log(data)
     return (
-    <div className="ms-4 me-4 mt-5 mb-5 border body-container">
+    <div className="ms-4 me-4 mt-5 mb-5 body-container">
         <h1>Images Repository</h1>
-        <div className="border">
-        <button type="submit" className="btn btn-success">Create</button>
+        <div className="mt-5 mb-5 d-flex justify-content-center">
+          <button type="submit" className="btn btn-success">Add Image</button>
         </div>
-        <div className="border card-container d-flex p-2 justify-content-around">
+        <div className="card-container d-flex p-2 justify-content-around flex-wrap">
           {
             data.map(image =>(
-              <div key={image.id} className="card card-style border di-flex align-items-center">
-                <div className='card-image-container mt-3'>
+              <div key={image.id} className="card card-style di-flex align-items-center mb-5">
+                <div className='card-image-container mt-3 mb-1'>
                   <img src={image.image} className="card-img-top card-image" alt={image.name}/>
                 </div>
-                <div className="card-body border">
-                  <h5 className="card-title">{image.name}</h5>
-                  <div class="mb-3 form-check">
-                    <input type="checkbox" className="bi bi-star" id="exampleCheck1"/>
+                <div className="card-body">
+                  <div className="d-flex justify-content-around">
+                    <div className="card-buttons d-flex justify-content-start punctuation-icon">
+                      <StarIcon size={30} fill="#ffca00" />
+                    </div>
+                    <div className="card-buttons d-flex justify-content-around management-icon">
+                      <PencilIcon size={30} fill="#0077fd" />
+                      <TrashIcon size={30} fill="#CB3234" />
+                    </div>
                   </div>
-                  <div className="card-buttons d-flex justify-content-around border">
-                    <button type="submit" className="btn btn-primary">Edit</button>
-                    <button type="submit" className="btn btn-danger">Delete</button>
-                  </div>
+                  <h5 className="card-title mt-4">{image.name}</h5>
                 </div>
               </div>
             ))
